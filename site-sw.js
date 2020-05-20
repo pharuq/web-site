c5eb8a613bafa936407cac6e04c1b6443a402874
Ryoma Shindo <ryoma.shindo@repro.io>
2020-05-20 11:46:58 +0900

    fox

--- site-sw.js
+++ site-sw.js
@@ -1,6 +1,6 @@
 console.log('hello from site-sw.js')
-// typeof importScripts === "function" && importScripts('https://s3-ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.js');
-typeof importScripts === "function" && importScripts('https://web-sdk-test-fox-2.s3-ap-northeast-1.amazonaws.com/repro-sw.js');
+typeof importScripts === "function" && importScripts('https://s3-ap-northeast-1.amazonaws.com/ryoma.shindo-test-backet/repro-sw.min.js');
+// typeof importScripts === "function" && importScripts('https://web-sdk-test-fox-2.s3-ap-northeast-1.amazonaws.com/repro-sw.js');
 
 self.addEventListener('install', function(event) {
   console.log('foo bar');
